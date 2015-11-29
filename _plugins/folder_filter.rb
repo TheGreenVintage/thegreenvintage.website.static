e6e21331754dde3caca9da2f3665848228e1ab70
Ferran Basora <fcsonline@gmail.com>
2015-11-29 23:26:15 +0100

    Fix image sorting

--- _plugins/folder_filter.rb
+++ _plugins/folder_filter.rb
@@ -5,7 +5,9 @@ module Jekyll
       full_path = File.join(@context.registers[:site].config['source'], input)
       files = Dir.glob("#{full_path}/#{pattern}").map do |e|
         File.join(input, File.basename(e)) if File.file? e
-      end.compact.sort_by { |x| x[/\d+/].to_i }
+      end.compact.sort_by do |x|
+        x.gsub('_','')[/\d+/].to_i
+      end
       puts "Searching files in: '#{full_path}'... (found: #{files.size} items)"
       files
     end
