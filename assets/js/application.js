87b64b08474a86f77f4d601cdc1cb9bf1fd3c1da
Ferran Basora <fcsonline@gmail.com>
2016-02-18 20:42:54 +0100

    WIP

---
+++ assets/js/application.js
@@ -0,0 +1,25 @@
+---
+---
+
+{% include_relative jquery.js %}
+{% include_relative jquery-migrate.min.js %}
+
+{% include_relative jquery.js %}
+{% include_relative jquery-migrate.min.js %}
+{% include_relative jquery.form.min.js %}
+
+{% include_relative ddsmoothmenu.js %}
+{% include_relative jquery.themepunch.tools.min.js %}
+{% include_relative jquery.themepunch.revolution.min.js %}
+
+{% include_relative jquery.address-1.4.min.js %}
+{% include_relative jquery.easing.1.3.min.js %}
+{% include_relative jquery.easytabs.js %}
+{% include_relative jquery.fitvids.js %}
+{% include_relative jquery.isotope.min.js %}
+{% include_relative mediaelement-and-player.min.js %}
+
+
+{% include_relative scripts.js %}
+{% include_relative selectnav.js %}
+{% include_relative zeta-slider.js %}
