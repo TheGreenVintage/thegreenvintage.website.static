60f98940c8d519dcc6644b67c16a909e23e81402
Ferran Basora <fcsonline@gmail.com>
2016-03-30 21:42:49 +0200

    New copies

--- _tools/downloadcsv.sh
+++ _tools/downloadcsv.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 echo "Downloading CSV files..."
-wget -q -O "../_data/portfolio.csv" "https://docs.google.com/spreadsheets/d/13uSnYS9xqWQiuCaZC8-gopAV7n3gx3Mj5tJn31FJSTo/pub?gid=0&single=true&output=csv"
-wget -q -O "../_data/partners.csv"  "https://docs.google.com/spreadsheets/d/13uSnYS9xqWQiuCaZC8-gopAV7n3gx3Mj5tJn31FJSTo/pub?gid=1075509751&single=true&output=csv"
-wget -q -O "../_data/home.csv"      "https://docs.google.com/spreadsheets/d/13uSnYS9xqWQiuCaZC8-gopAV7n3gx3Mj5tJn31FJSTo/pub?gid=897764401&single=true&output=csv"
+# wget -q -O "../_data/portfolio.csv" "https://docs.google.com/spreadsheets/d/13uSnYS9xqWQiuCaZC8-gopAV7n3gx3Mj5tJn31FJSTo/pub?gid=0&single=true&output=csv"
+# wget -q -O "../_data/partners.csv"  "https://docs.google.com/spreadsheets/d/13uSnYS9xqWQiuCaZC8-gopAV7n3gx3Mj5tJn31FJSTo/pub?gid=1075509751&single=true&output=csv"
+# wget -q -O "../_data/home.csv"      "https://docs.google.com/spreadsheets/d/13uSnYS9xqWQiuCaZC8-gopAV7n3gx3Mj5tJn31FJSTo/pub?gid=897764401&single=true&output=csv"
+
+wget -q -O "../_data/destionations.csv" "https://docs.google.com/spreadsheets/d/1e2hsPriBILVB0pZzZketUhcRAJq7IuYNAb-al3HbxjQ/pub?gid=1539468359&single=true&output=csv"
+wget -q -O "../_data/activities.csv"    "https://docs.google.com/spreadsheets/d/1e2hsPriBILVB0pZzZketUhcRAJq7IuYNAb-al3HbxjQ/pub?gid=802183433&single=true&output=csv"
 
 dos2unix ../_data/*
--- _tools/downloadi18n.sh
+++ _tools/downloadi18n.sh
@@ -3,5 +3,5 @@ FILE="/tmp/thegreenvintage.i18n.csv"
 
 echo "Downloading i18n CSV file..."
 rm -f ${FILE}
-wget -q -O ${FILE} "https://docs.google.com/spreadsheets/d/13uSnYS9xqWQiuCaZC8-gopAV7n3gx3Mj5tJn31FJSTo/pub?gid=1694880076&single=true&output=csv"
+wget -q -O ${FILE} "https://docs.google.com/spreadsheets/d/1e2hsPriBILVB0pZzZketUhcRAJq7IuYNAb-al3HbxjQ/pub?gid=0&single=true&output=csv"
 ruby ../_tools/csv2yaml.rb ${FILE}
