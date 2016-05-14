a93d142edfce27b09c05e7b6b1dedbd8f115ea9c
Ferran Basora <fcsonline@gmail.com>
2016-05-14 20:52:13 +0200

    WIP

--- _tools/downloadi18n.sh
+++ _tools/downloadi18n.sh
@@ -3,5 +3,6 @@ FILE="/tmp/thegreenvintage.i18n.csv"
 
 echo "Downloading i18n CSV file..."
 rm -f ${FILE}
+
 wget -q -O ${FILE} "https://docs.google.com/spreadsheets/d/1e2hsPriBILVB0pZzZketUhcRAJq7IuYNAb-al3HbxjQ/pub?gid=0&single=true&output=csv"
 ruby ../_tools/csv2yaml.rb ${FILE}
