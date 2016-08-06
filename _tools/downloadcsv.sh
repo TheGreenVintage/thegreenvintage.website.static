9b90551dabea8447ed50f03aaba0f7293d50d177
Ferran Basora <fcsonline@gmail.com>
2016-08-06 16:50:02 +0200

    Fix employee

--- _tools/downloadcsv.sh
+++ _tools/downloadcsv.sh
@@ -3,7 +3,7 @@
 echo "Downloading CSV files..."
 wget -q -O "../_data/destinations.csv" "https://docs.google.com/spreadsheets/d/1DXPTtb1tN-4HCwsLhMGICMxQrg8b28w1CnMAhT90rFo/pub?gid=1539468359&single=true&output=csv"
 wget -q -O "../_data/activities.csv"   "https://docs.google.com/spreadsheets/d/1DXPTtb1tN-4HCwsLhMGICMxQrg8b28w1CnMAhT90rFo/pub?gid=802183433&single=true&output=csv"
-wget -q -O "../_data/employees.csv"    "https://docs.google.com/spreadsheets/d/1DXPTtb1tN-4HCwsLhMGICMxQrg8b28w1CnMAhT90rFo/pub?gid=261678603&single=true&output=cs"
+wget -q -O "../_data/employees.csv"    "https://docs.google.com/spreadsheets/d/1DXPTtb1tN-4HCwsLhMGICMxQrg8b28w1CnMAhT90rFo/pub?gid=261678603&single=true&output=csv"
 wget -q -O "../_data/home.csv"         "https://docs.google.com/spreadsheets/d/1DXPTtb1tN-4HCwsLhMGICMxQrg8b28w1CnMAhT90rFo/pub?gid=125310584&single=true&output=csv"
 
 dos2unix ../_data/*
