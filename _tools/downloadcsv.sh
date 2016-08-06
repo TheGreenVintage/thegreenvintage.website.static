a08bbc3523d909642fdb830086043ae51024807b
Ferran Basora <fcsonline@gmail.com>
2016-08-06 14:26:09 +0200

    Teambuilding

--- _tools/downloadcsv.sh
+++ _tools/downloadcsv.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 echo "Downloading CSV files..."
-wget -q -O "../_data/destinations.csv" "https://docs.google.com/spreadsheets/d/1e2hsPriBILVB0pZzZketUhcRAJq7IuYNAb-al3HbxjQ/pub?gid=1539468359&single=true&output=csv"
-wget -q -O "../_data/activities.csv"   "https://docs.google.com/spreadsheets/d/1e2hsPriBILVB0pZzZketUhcRAJq7IuYNAb-al3HbxjQ/pub?gid=802183433&single=true&output=csv"
-wget -q -O "../_data/employees.csv"    "https://docs.google.com/spreadsheets/d/1e2hsPriBILVB0pZzZketUhcRAJq7IuYNAb-al3HbxjQ/pub?gid=261678603&single=true&output=csv"
-wget -q -O "../_data/tours.csv"        "https://docs.google.com/spreadsheets/d/1e2hsPriBILVB0pZzZketUhcRAJq7IuYNAb-al3HbxjQ/pub?gid=2078551218&single=true&output=csv"
-wget -q -O "../_data/home.csv"         "https://docs.google.com/spreadsheets/d/1e2hsPriBILVB0pZzZketUhcRAJq7IuYNAb-al3HbxjQ/pub?gid=125310584&single=true&output=csv"
+wget -q -O "../_data/destinations.csv" "https://docs.google.com/spreadsheets/d/1DXPTtb1tN-4HCwsLhMGICMxQrg8b28w1CnMAhT90rFo/pub?gid=1539468359&single=true&output=csv"
+wget -q -O "../_data/activities.csv"   "https://docs.google.com/spreadsheets/d/1DXPTtb1tN-4HCwsLhMGICMxQrg8b28w1CnMAhT90rFo/pub?gid=802183433&single=true&output=csv"
+wget -q -O "../_data/employees.csv"    "https://docs.google.com/spreadsheets/d/1DXPTtb1tN-4HCwsLhMGICMxQrg8b28w1CnMAhT90rFo/pub?gid=261678603&single=true&output=cs"
+wget -q -O "../_data/home.csv"         "https://docs.google.com/spreadsheets/d/1DXPTtb1tN-4HCwsLhMGICMxQrg8b28w1CnMAhT90rFo/pub?gid=125310584&single=true&output=csv"
 
 dos2unix ../_data/*
