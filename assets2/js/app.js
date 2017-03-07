98b26127ccdea914001773da53478d4dddfd0a92
Ferran Basora <fcsonline@gmail.com>
2017-03-07 23:25:13 +0100

    Fix contact

--- assets2/js/app.js
+++ assets2/js/app.js
@@ -1,26 +1,52 @@
 //bizwrap v1.3
 
+/*-----------------------------------------------------------------------------------*/
+/*	CONTACT FORM
+/*-----------------------------------------------------------------------------------*/ 
+$(document).ready(function() {
+  var form = $('.contact-form form');
 
+  form.find('input[type=text]').first().focus();
+
+  form.submit(function() {
+
+    var postdata = form.serialize();
+
+    $.ajax({
+      type: 'POST',
+      url: form.attr('action'),
+      data: postdata,
+      dataType: 'json'
+    }).fail(function(error){
+      alert('Error:' + error);
+    }).done(function(data){
+      window.location = form.data('success');
+    });
+
+    return false;
+  });
+});
 
 $( window ).resize(function() {
-    $(".navbar-collapse").css({ maxHeight: $(window).height() - $(".navbar-header").height() + "px" });
+  $(".navbar-collapse").css({ maxHeight: $(window).height() - $(".navbar-header").height() + "px" });
 });
+
 /* ==============================================
- Sticky Navbar
- =============================================== */
+   Sticky Navbar
+   =============================================== */
 
 $(document).ready(function() {
-    $(".sticky").sticky({topSpacing: 0});
-    
-    //search trigger
-    jQuery('.icon-search, .search-close').click(function () {
-        jQuery('.search-form-nav').animate({height: 'toggle'}, 500);
-    });
-    
-    $(function() {
-  $('.smooth-scroll a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
+  $(".sticky").sticky({topSpacing: 0});
+
+  //search trigger
+  $('.icon-search, .search-close').click(function () {
+    $('.search-form-nav').animate({height: 'toggle'}, 500);
+  });
+
+  $(function() {
+    $('.smooth-scroll a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
         $('html, body').animate({
@@ -29,31 +55,28 @@ $(document).ready(function() {
         return false;
       }
     }
+    });
   });
+
 });
-    
-});
-
-
-
 
 /*=========================*/
 /*====main navigation hover dropdown====*/
 /*==========================*/
 $(document).ready(function() {
 
-    $('.js-activated').dropdownHover({
-        instantlyCloseOthers: false,
-        delay: 0
-    }).dropdown();
+  $('.js-activated').dropdownHover({
+    instantlyCloseOthers: false,
+    delay: 0
+  }).dropdown();
 
 });
 
 /*====flex  slider for main slider or testimonials====*/
 $('.main-flex-slider,.testimonials').flexslider({
-    slideshowSpeed: 5000,
-    directionNav: false,
-    animation: "fade"
+  slideshowSpeed: 5000,
+  directionNav: false,
+  animation: "fade"
 });
 
 /*========portfolio mix====*/
@@ -68,32 +91,32 @@ $("[data-toggle=tooltip]").tooltip();
 
 /*========flex-gallery slide====*/
 $(window).load(function() {
-    $('.flexslider').flexslider({
-        directionNav: false,
-        slideshowSpeed: 3000,
-        animation: "fade"
-    });
+  $('.flexslider').flexslider({
+    directionNav: false,
+    slideshowSpeed: 3000,
+    animation: "fade"
+  });
 });
 
 /*=========================*/
-     /*========Animation on scroll with wow.js====*/
-     /*==========================*/
-     
-   wow = new WOW(
-      {
-        animateClass: 'animated',
-        offset:       100,
-        mobile:       true
-      }
+/*========Animation on scroll with wow.js====*/
+/*==========================*/
+
+wow = new WOW(
+    {
+      animateClass: 'animated',
+      offset:       100,
+      mobile:       true
+    }
     );
-    wow.init();
-    
-    /* -------------------
- Parallax Sections
- ---------------------*/
+wow.init();
+
+/* -------------------
+   Parallax Sections
+   ---------------------*/
 if (!Modernizr.touch) {
-    $('.parallax-1').parallax("50%", 0.5);
-    $('.page-tree-bg').parallax("50%", 0.5);
+  $('.parallax-1').parallax("50%", 0.5);
+  $('.page-tree-bg').parallax("50%", 0.5);
 }
 
 $(window).load(function() {
